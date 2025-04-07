89e7e83bcdb93c0813401645e8908b5d8b4c8d45
LAPTOP-UVB7VO2H\hp <andreadepadova43@gmail.com>
2025-04-07 15:03:36 +0200

    uso Observable per lista prodotti

--- src/app/carrello/servizio-carrello.service.ts
+++ src/app/carrello/servizio-carrello.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { CarrelloComponent } from './carrello.component';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServizioCarrelloService {
-  
-  listaProdottiAggiunti: string[] = []; 
+
+  private listaProdottiAggiuntiSubject = new BehaviorSubject<string[] | []>([]);
+  listaProdottiAggiunti$: Observable<string[]> = this.listaProdottiAggiuntiSubject.asObservable();
+
+  listaProdottiAggiunti: string[] = [];
 
   constructor() { }
 
   aggiungiProdotto(name: string) {
     this.listaProdottiAggiunti.push(name);
+    this.listaProdottiAggiuntiSubject.next(this.listaProdottiAggiunti); 
   }
 
   rimuoviProdotto(name: string): boolean {
     const index = this.listaProdottiAggiunti.indexOf(name);
     if (index > -1) {
       this.listaProdottiAggiunti.splice(index, 1);
+      this.listaProdottiAggiuntiSubject.next(this.listaProdottiAggiunti); 
       return true;
     }
     return false;
