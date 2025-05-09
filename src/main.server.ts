7db2b6054fa74c1a93c92c9af643252125e1ea58
LAPTOP-UVB7VO2H\hp <andreadepadova43@gmail.com>
2025-05-09 17:36:48 +0200

    aggiungo getPrerenderParams per risolvere l'errore

--- src/main.server.ts
+++ src/main.server.ts
@@ -5,3 +5,24 @@ import { config } from './app/app.config.server';
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
+
+export const getPrerenderParams = () => {
+    const prodotti = {
+      telefoni: ['iphone-15', 'galaxy-s23'],
+      televisori: ['lg-oled', 'samsung-qled'],
+      pc: ['macbook-pro', 'dell-xps'],
+    } as const;
+  
+    const routes: { route: string }[] = [];
+  
+    for (const categoria of Object.keys(prodotti) as (keyof typeof prodotti)[]) {
+      routes.push({ route: `/prodotti/${categoria}` });
+  
+      for (const nome of prodotti[categoria]) {
+        routes.push({ route: `/prodotti/${categoria}/${nome}` });
+      }
+    }
+  
+    return routes;
+};
+  
