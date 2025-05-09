4b25ee5f2ef69c0f99f0c47fc12a402aec6444be
LAPTOP-UVB7VO2H\hp <andreadepadova43@gmail.com>
2025-05-09 16:50:31 +0200

    tento di risolvere l'errore nel deployment tramite una modifica nelle routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { HomePageProductComponent } from './home-page-product/home-page-product.component';
 import { CarrelloComponent } from './carrello/carrello.component';
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -14,9 +15,9 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent}, 
     {path: 'registrazione', component: RegistrazioneComponent},
     // Unica route per tutti i prodotti
-    {path: 'prodotti/:categoria', component: HomePageProductComponent},
-    {path: 'prodotti/telefoni/:name', component: TelefoniComponent},
-    {path: 'prodotti/televisori/:name', component: TelevisoriComponent},
-    {path: 'prodotti/pc/:name', component: PcComponent},
+    {path: 'prodotti/:categoria', component: HomePageProductComponent, data: { RenderMode: 'server' }},
+    {path: 'prodotti/telefoni/:name', component: TelefoniComponent, data: { RenderMode: 'server' }},
+    {path: 'prodotti/televisori/:name', component: TelevisoriComponent, data: { RenderMode: 'server' }},
+    {path: 'prodotti/pc/:name', component: PcComponent, data: { RenderMode: 'server' }},
     {path: 'carrello', component: CarrelloComponent}
 ];
